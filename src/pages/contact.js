509a2f169c085100e8b10ad0b6c2d51ac47c48f5
isabelle-np <your_email@example.com>
2020-01-15 13:45:31 -0500

    Added about me details

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,54 @@ import React from 'react';
 import Layout from '../components/layout';
 import Title from '../components/title';
 
-export default () => (
-    <Layout>
+class Contact extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: '',
+        email: '',
+        message: ''
+      }
+    }
+
+    render () {
+        return (
+            <Layout>
         <Title title="Contact" subtitle="Think I'm a good fit for your team? I'd love to talk."></Title>
+        <div>
+        <form id="contact-form" /*onSubmit={this.handleSubmit.bind(this)}*/ method="POST">
+    <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input type="text" className="form-control" /*value={this.state.name} onChange={this.onNameChange.bind(this)}*/ />
+    </div>
+    <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Email address</label>
+        <input type="email" className="form-control" aria-describedby="emailHelp" /*value={this.state.email} onChange={this.onEmailChange.bind(this)}*/ />
+    </div>
+    <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea className="form-control" rows="5" /*value={this.state.message} onChange={this.onMessageChange.bind(this)}*/ />
+    </div>
+    <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+        </div>
     </Layout>
-)
+        )
+    }
+
+    onNameChange(event) {
+        this.setState({name: event.target.value})
+      }
+    
+      onEmailChange(event) {
+        this.setState({email: event.target.value})
+      }
+    
+      onMessageChange(event) {
+        this.setState({message: event.target.value})
+      }
+    
+    handleSubmit(event) {
+    }
+}
+export default Contact;

db0c504682550491908be3aa6651aa7ca0bf1d21
isabelle-np <your_email@example.com>
2020-01-17 11:49:32 -0500

    Updates

--- src/components/title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './title.module.scss'
-
-export default ({ title, subtitle }) => (
-    <section className={styles.container}>
-        <h1 className={styles.title}>{title}</h1>
-        <p className={styles.subtitle}>{subtitle}</p>
-    </section>
-)

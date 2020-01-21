2bbac88236e59e04bf4fade449fabb551dcb8eba
isabelle-np <your_email@example.com>
2020-01-21 09:03:30 -0500

    Fixed ESLint issues

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 export default () => (
     <footer className={styles.footer}>    
         <nav className="social-media">
-            <a href="http://linkedin.com/in/isabellenguyenphuoc/" target="_blank" aria-label="LinkedIn">
+            <a href="http://linkedin.com/in/isabellenguyenphuoc/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
                 <FontAwesomeIcon icon={['fab','linkedin']} aria-hidden="true" className={styles.icon}/> 
             </a>
 
@@ -15,7 +15,7 @@ export default () => (
                 <FontAwesomeIcon icon="envelope-square" aria-hidden="true" className={styles.icon}/> 
             </a>
   
-            <a href="https://github.com/isabelle-np" target="_blank" aria-label="Github">
+            <a href="https://github.com/isabelle-np" target="_blank" rel="noopener noreferrer" aria-label="Github">
                 <FontAwesomeIcon icon={['fab', 'github-square']} aria-hidden="true" className={styles.icon}/>
             </a>
         </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,12 @@ import { Link } from "gatsby";
 export default ({data}) => (
     <Layout>
         <div className={styles.container}>
-            <img src={Logo} alt="Logo" className={styles.logo}/>
+            <img src={Logo} className={styles.logo} alt="Lightbulb logo"/>
             <h1 className={styles.name}>Isabelle<span className={styles.lastName}>Nguyen-Phuoc</span></h1>
             <h2 className={styles.subtitle}>Software Engineer &amp; Designer</h2>
             <span className={styles.description}>Translating clients’ needs into UX solutions <br/>from idea, to mockup, to production-ready code</span>
             <Link to='/#portfolio' className={styles.arrow}>
-                <img className={styles.animation} src="https://d3n32ilufxuvd1.cloudfront.net/55c4887f2541c97e1b608fee/1341550/upload-a74f3e75-fba2-43cb-967a-63dfb6cf214c.png"/>
+                <img className={styles.animation} src="https://d3n32ilufxuvd1.cloudfront.net/55c4887f2541c97e1b608fee/1341550/upload-a74f3e75-fba2-43cb-967a-63dfb6cf214c.png" alt="Link to Portfolio"/>
                 <span className={styles.caption}>View Portfolio</span>
 
             </Link>
